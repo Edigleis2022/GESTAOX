fcef7ae814437be34172c527399e4377076b47bc
Edigleis <105567136+Edigleis2022@users.noreply.github.com>
2025-12-03 04:42:53 -0400

    Primeiro Comit 03/12/2025 04:42
    Comit feito as 04:41, 03/12/2025 Projeto GestaoX.

---
+++ src/main/java/com/javanauta/cadastro_usuario/model/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.javanauta.cadastro_usuario.model.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.javanauta.cadastro_usuario.model.entitys.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
